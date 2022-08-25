a8d867bb4092c037a80127eb8dd0494ea97c0678
Eric R. Scott <scottericr@gmail.com>
2022-08-25 10:17:14 -0400

    Data validation in Excel and R (#15)
    Add materials for data validation workshop

---
+++ 202208-data-validation/make_data.R
@@ -0,0 +1,78 @@
+#' A script to take some data from 
+
+library(tidyverse)
+library(readxl)
+library(here)
+library(lubridate)
+
+# read in sample data
+df <- read_csv(here::here("202208-data-validation", "data_raw", "tea_leafhopper.csv"),
+               col_types = cols(
+                 Start.Time = col_character()
+               ))
+
+df <-
+  df %>%
+  rename(time = Start.Time, plant_id = Plant) %>% 
+  rename_with(~str_replace(., "S", "shoot_")) %>% 
+  rename_with(tolower) %>% 
+  mutate(leaves = case_when(
+    date %in% c("Jul 1", "Jul 2") ~ 30,
+    TRUE ~ 50
+  )) %>% 
+  mutate(date = mdy(paste(date, "2016"))) %>% 
+  #make plant_id unique
+  mutate(plant_id = if_else(field == "B", plant_id + 10, plant_id))
+
+# create "mistakes" in the "true" data
+
+df <- df %>% 
+  #add a suspiciously high number of leaf hoppers per leaf
+  mutate(hoppers = if_else(row_number() %in% c(4, 57), 15, hoppers)) %>% 
+  #add a different counter for one day
+  mutate(counter = if_else(row_number() == 5, "X", counter)) %>% 
+  #change a shoot to be 0 cm (should have marked as NA)
+  mutate(shoot_6 = if_else(row_number() == 55, 0, shoot_6)) %>% 
+  #add decimal in hoppers column
+  mutate(hoppers = if_else(row_number() == 13, hoppers + 0.1, hoppers))
+  
+
+
+df_eric <- df_jessica <- df
+
+# Create data entry errors in Eric's version
+
+df_eric <-
+  df_eric %>% 
+  mutate(
+    # read a 7 as a 1
+    shoot_4 = if_else(row_number() == 10, 1, shoot_4),
+    # enter a trailing space
+    counter = if_else(row_number() == 29, "E ", counter)
+  ) %>% 
+  # missed the last row
+  mutate(
+    across(c(starts_with("shoot_"), plant_id, counter, leaves, hoppers), ~ifelse(row_number() == 60, NA, .))
+  )
+  
+
+# Data entry errors in Jessica's version
+
+df_jessica <-
+  df_jessica %>% 
+  mutate(
+    # swapped two values
+    shoot_7 = if_else(row_number() == 27, 3.5, shoot_7),
+    shoot_7 = if_else(row_number() == 28, 3.2, shoot_7),
+    # forget to enter a decimal
+    shoot_3 = if_else(row_number() == 48, shoot_3*10, shoot_3),
+    # typo
+    plant_id = if_else(row_number() == 50, 11, plant_id)
+  )
+
+anti_join(df_eric, df_jessica)
+anti_join(df_jessica, df_eric)
+
+write_csv(df_eric, here("202208-data-validation", "data", "tea_eric.csv"))
+write_csv(df_jessica, here("202208-data-validation", "data", "tea_jessica.csv"))
+write_csv(df, here("202208-data-validation", "data", "tea_resolved.csv"))
