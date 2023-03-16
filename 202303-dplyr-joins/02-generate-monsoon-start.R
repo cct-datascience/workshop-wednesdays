379d8d25f1d68f929415f8c2da5a79bc08c82ee2
Jessica Guo <jessicaguo@email.arizona.edu>
2023-03-16 13:24:08 -0700

    Add dataset for non-equi join example

---
+++ 202303-dplyr-joins/02-generate-monsoon-start.R
@@ -0,0 +1,39 @@
+# Obtain rainfall data near portal
+# Calculate monsoon start dates
+# Create table for non-equi joins
+library(tidyverse)
+
+dat_url <- "https://zenodo.org/record/7735298/files/weecology/PortalData-4.0.0.zip"
+dat_path <- "202303-dplyr-joins/data_raw/portaldata.zip"
+
+download.file(dat_url, destfile = dat_path)
+unzip(dat_path, exdir = "202303-dplyr-joins/data_raw")
+
+weather <- read_csv("202303-dplyr-joins/data_raw/weecology-PortalData-6017f65/Weather/Portal_weather.csv")
+# error can be ignored, not using `soiltemp`
+str(weather)
+
+#### Calculate monsoon start ####
+# sum to daily
+# calculate cumulative JAS
+mstart <- weather %>%
+  mutate(date = as.Date(timestamp)) %>%
+  group_by(date) %>%
+  summarize(year = unique(year),
+            month = unique(month),
+            ppt = sum(precipitation)) %>%
+  ungroup() %>%
+  filter(month %in% 7:9) %>%
+  group_by(year) %>%
+  mutate(cumppt = cumsum(ppt),
+         totppt = sum(ppt),
+         tenthppt = 0.1*totppt,
+         exceed = ifelse(cumppt > tenthppt, 1, 0)) %>%
+  summarize(m_start = date[min(which(exceed == 1))]) %>%
+  ungroup() %>%
+  mutate(m_end = as.Date(paste0(year, "-09-30")))
+  
+
+# Save out
+write_csv(mstart,"202303-dplyr-joins/data_clean/portal-monsoon.csv")
+
---
+++ 202303-dplyr-joins/03-nonequi-join.R
@@ -0,0 +1,21 @@
+# Label each survey observation by 
+# whether or not the rodent was captured during monsoon season
+# Monsoon start is defined as the day on which 
+# 10% of the total JAS precip is achieved
+
+
+library(tidyverse)
+
+# Read in surveys data
+surveys_complete <- read_csv("202303-dplyr-joins/data_clean/surveys_complete.csv")
+monsoon <- read_csv("202303-dplyr-joins/data_clean/portal-monsoon.csv")
+
+str(surveys_complete)
+str(monsoon)
+
+# Create date column
+# Use with join_by() function tha thas more flexible specification
+
+surveys_monsoon <- surveys_complete %>%
+  mutate(date = as.Date(paste0(year, "-", month, "-", day))) %>%
+  right_join(monsoon, join_by(date >= m_start, date <= m_end, year))
