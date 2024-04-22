314fbac0c9d6eaf01ffbfc713e4ac9cf11501c9f
Renata Diaz <renatadiaz@arizona.edu>
2024-04-22 13:30:05 -0700

    Slides for April workshop.

--- 202404-big-data/save_hive.R
+++ 202404-big-data/save_hive.R
@@ -6,7 +6,12 @@ birds_arrow_dataset <-
     sources = here::here("202404-big-data",
                          "data",
                          "all_routes_all_stops.csv"),
-    format = "csv")
+    format = "csv") |>
+  select(StateNum, Route, Year, AOU, StopTotal) |>
+  collect() 
 
 write_csv_dataset(birds_arrow_dataset, here::here("202404-big-data", "data", "hive"),
                   partitioning = "StateNum")
+write_csv_arrow(birds_arrow_dataset, here::here("202404-big-data",
+                                                "data",
+                                                "all_routes_all_stops.csv"))
